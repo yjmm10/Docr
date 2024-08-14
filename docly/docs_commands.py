0d111cb12bac294e960bc15cf005778f6c14fa34
liferecords <yjmm10@yeah.net>
2024-08-14 21:31:36 +0800

    remove file

--- docly/docs_commands.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-def serve():
-    subprocess.run(["mkdocs", "serve"], check=True)
-
-def build():
-    subprocess.run(["mkdocs", "build"], check=True)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
 """Test"""
+
 from pathlib import Path
 
 from docly.config import settings
